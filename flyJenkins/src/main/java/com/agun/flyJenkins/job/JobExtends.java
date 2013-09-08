4e49ab12dcd814f19c4efb9868baf6c1a636b008
agune <pdc222@nate.com>
2013-09-08 17:54:03 +0900

    build & deploy 연동 테스트

--- flyJenkins/src/main/java/com/agun/flyJenkins/job/JobExtends.java
+++ flyJenkins/src/main/java/com/agun/flyJenkins/job/JobExtends.java
@@ -1,15 +1,22 @@
 package com.agun.flyJenkins.job;
 
 
+import java.io.File;
+import java.io.IOException;
+
 import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
 
 import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
 import hudson.model.Action;
+import hudson.model.BuildListener;
 import hudson.model.JobProperty;
 import hudson.model.JobPropertyDescriptor;
+import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 
 public class JobExtends extends JobProperty<AbstractProject<?,?>>  {
@@ -29,6 +36,32 @@ public class JobExtends extends JobProperty<AbstractProject<?,?>>  {
 		return new JobAction(this.owner.getName());
 	}
 
+	@Override
+	public boolean perform(AbstractBuild<?,?> build, Launcher launcher, BuildListener listener){
+		System.out.println("build result: " + build.getResult());
+		System.out.println("display name : " + build.getDisplayName());
+	
+		
+		try {
+			FilePath filePath = new FilePath(new File("/Users/pdc222/work/test.zip"));
+			FilePath filePath2 = new FilePath(new File("/Users/pdc222/work/a.war"));
+			filePath.unzip(filePath2);
+			//FilePath filePath2 = new FilePath(new File(production));
+			//filePath2.zip(filePath);
+			
+			for(FilePath file : build.getWorkspace().list()){
+				System.out.println("name :  "  + file.getName());
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
+		
+	}
 	
 	@Extension
 	public static final class DescriptorImpl extends JobPropertyDescriptor {
