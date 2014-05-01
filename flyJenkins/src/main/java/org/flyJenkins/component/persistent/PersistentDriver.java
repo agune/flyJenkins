c09f92afe1398ff530644a754606d68521f18e4e
agune <pdc222@nate.com>
2014-05-01 19:56:24 +0900

    exception 잘못된거 계속 ㅜㅜ

--- flyJenkins/src/main/java/org/flyJenkins/component/persistent/PersistentDriver.java
+++ flyJenkins/src/main/java/org/flyJenkins/component/persistent/PersistentDriver.java
@@ -7,9 +7,6 @@ package org.flyJenkins.component.persistent;
 
 import java.sql.Connection;
 import java.sql.Statement;
-import java.util.List;
-
-import com.sun.tools.corba.se.idl.InvalidArgument;
 
 public interface PersistentDriver {
 	public void initDevice();
